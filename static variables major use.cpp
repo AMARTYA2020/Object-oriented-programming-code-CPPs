d6bff3ec6860b8f06c6abbe4b0e52683fe8bfec4
Amartya pandey <amartyapandey19@gmail.com>
2021-04-22 08:11:46 +0530

    Add files via upload

---
+++ static variables major use.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+void display();
+int main()
+{
+    display();
+    display();
+    display();
+    return 0;
+}
+
+void display()
+{
+    static int counter=0;
+    cout<<"Display function called"<<++counter<<"times"<<endl;
+    cout<<"Static keyword is used inside a function that variable will be availablle to fuction calls"<<endl;
+
+}
